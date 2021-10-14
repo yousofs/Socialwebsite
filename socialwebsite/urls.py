ef97b5a20df0f904f405a7cdcaf0de071360a56f
Yousof Shahrabi <yousof.shahrabi84@gmail.com>
2021-10-14 18:36:02 +0330

    add login/logout/register page

--- socialwebsite/urls.py
+++ socialwebsite/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
 
+app_name = 'main'
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('posts.urls', namespace='posts'))
+    path('', include('posts.urls', namespace='posts')),
+    path('account/', include('account.urls', namespace='account')),
 ]
