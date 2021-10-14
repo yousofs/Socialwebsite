4062cc707d203e0290a2940804ff7d7b480111f7
Yousof Shahrabi <yousof.shahrabi84@gmail.com>
2021-10-14 17:29:09 +0330

    add posts and post_detail page

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.urls import reverse
 
-# Create your models here.
+
+class Post(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    body = models.TextField(max_length=500)
+    slug = models.SlugField(max_length=200)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.user} - {self.body[:20]}'
+
+    def get_absolute_url(self):
+        return reverse('posts:post_detail', args=[self.created.year, self.created.month, self.created.day, self.slug])
---
+++ posts/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+app_name = 'posts'
+urlpatterns = [
+    path('', views.all_posts, name='all_posts'),
+    path('<int:year>-<int:month>-<int:day>-<slug:slug>/', views.post_detail, name='post_detail')
+]
