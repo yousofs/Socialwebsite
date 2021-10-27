75e759e16f32de3df1fe2b8816d4dc13690e9554
Yousof Shahrabi <yousof.shahrabi84@gmail.com>
2021-10-27 13:52:40 +0330

    comment reply added

--- posts/forms.py
+++ posts/forms.py
@@ -27,3 +27,17 @@ class AddCommentForm(forms.ModelForm):
         widgets = {
             'body': forms.Textarea(attrs={'class': 'form-control'})
         }
+        error_messages = {
+            'body': {
+                'required': 'این فیلد اجباری است',
+            }
+        }
+        help_texts = {
+            'body': 'max 400 char'
+        }
+
+
+class AddReplyForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ('body',)
--- posts/views.py
+++ posts/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from .models import Post, Comment
 from django.shortcuts import get_object_or_404
-from .forms import AddPostForm, EditPostForm, AddCommentForm
+from .forms import AddPostForm, EditPostForm, AddCommentForm, AddReplyForm
 from django.contrib import messages
 from django.utils.text import slugify
 from django.contrib.auth.decorators import login_required
@@ -15,6 +15,7 @@ def all_posts(request):
 def post_detail(request, year, month, day, slug):
     post = get_object_or_404(Post, created__year=year, created__month=month, created__day=day, slug=slug)
     comments = Comment.objects.filter(post=post, is_reply=False).order_by('-created')
+    reply_form = AddReplyForm()
     if request.method == 'POST':
         form = AddCommentForm(request.POST)
         if form.is_valid():
@@ -25,7 +26,8 @@ def post_detail(request, year, month, day, slug):
             messages.success(request, 'Your comment submitted successfully', 'success')
     else:
         form = AddCommentForm()
-    return render(request, 'posts/post-detail.html', {'post': post, 'comments': comments, 'form': form})
+    return render(request, 'posts/post-detail.html',
+                  {'post': post, 'comments': comments, 'form': form, 'reply': reply_form})
 
 
 @login_required
@@ -74,3 +76,20 @@ def post_edit(request, user_id, post_id):
         return render(request, 'posts/edit_post.html', {'form': form})
     else:
         return redirect('posts:all_posts')
+
+
+@login_required()
+def add_reply(request, post_id, comment_id):
+    post = get_object_or_404(Post, id=post_id)
+    comment = get_object_or_404(Comment, pk=comment_id)
+    if request.method == 'POST':
+        form = AddReplyForm(request.POST)
+        if form.is_valid():
+            reply = form.save(commit=False)
+            reply.user = request.user
+            reply.post = post
+            reply.reply = comment
+            reply.is_reply = True
+            reply.save()
+            messages.success(request, 'Your reply submitted successfully', 'success')
+    return redirect('posts:post_detail', post.created.year, post.created.month, post.created.day, post.slug)
